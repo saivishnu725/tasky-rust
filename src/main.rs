1166593e62c3cbaaaace5f3d14868e1efdda95f7
saivishnu725 <saivishnu725@gmail.com>
2025-02-11 18:49:13 +0530

    [RS-1] create task structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+struct Task {
+    active: bool,
+    title: String,
+    description: String,
+    // TODO: use an appropriate type for Date
+    updated_on: String,
+    created_on: String,
+    // TODO: placeholder for when multiple users can co-exist
+    // created_by: String,
+    // updated_by: String
 }
+
+fn main() {}
