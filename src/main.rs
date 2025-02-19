f9854f2d5855fb03b426767356d3b61249125c1b
Sai Vishnu <saivishnu725@gmail.com>
2025-02-19 13:19:50 +0530

    [RS-2] load tasks from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use csv::Reader;
 use serde::{Deserialize, Serialize};
-use std::fs;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Task {
@@ -10,23 +13,32 @@ struct Task {
     updated_on: String,
     created_on: String,
     // TODO: placeholder for when multiple users can co-exist
-    // created_by: String,
-    // updated_by: String
+    #[serde(default)] // default if empty
+    created_by: String,
+    #[serde(default)] // default if empty
+    updated_by: String,
+}
+
+fn load_tasks(file_path: &str) -> Result<Vec<Task>, Box<dyn Error>> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let mut csv_reader = Reader::from_reader(reader);
+
+    let mut tasks = Vec::new();
+    for result in csv_reader.deserialize() {
+        let task: Task = result?;
+        tasks.push(task);
+    }
+
+    Ok(tasks)
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let file_path: &str = "tmp/tasks.json";
-    let data_file = fs::read_to_string(file_path).expect("Error reading file: ");
-    let mut data: Task = serde_json::from_str(&data_file).unwrap();
-
-    println!("{:?}", data);
-
-    if data.active {
-        data.active = false;
-    } else {
-        data.active = true;
-    }
-    fs::write(file_path, serde_json::to_string_pretty(&data).unwrap()).expect("error writing: ");
-
+    // will be in a config file later
+    // or heck, it'll be a database so no need for a csv file at all
+    let file_path: &str = "tmp/tasks.csv";
+    let tasks = load_tasks(file_path).expect("Failed comprehending the csv: ");
+    println!("{:?}", tasks);
+    println!("first: {:?}", tasks[1]);
     Ok(())
 }
