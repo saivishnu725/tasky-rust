e5a0248463b2372ca80b505f094d8a1787ee2d49
Sai Vishnu <saivishnu725@gmail.com>
2025-02-22 15:57:06 +0530

    [RS-18] add task to list

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use csv::Reader;
+use csv::{Reader, Writer};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
+use std::vec;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Task {
@@ -33,12 +34,34 @@ fn load_tasks(file_path: &str) -> Result<Vec<Task>, Box<dyn Error>> {
     Ok(tasks)
 }
 
+fn add_task(tasks: &mut Vec<Task>, new_task: Task) -> Result<(), Box<dyn Error>> {
+    tasks.push(new_task);
+    Ok(())
+}
+
 fn main() -> Result<(), std::io::Error> {
     // will be in a config file later
     // or heck, it'll be a database so no need for a csv file at all
     let file_path: &str = "tmp/tasks.csv";
-    let tasks = load_tasks(file_path).expect("Failed comprehending the csv: ");
+    let mut tasks = load_tasks(file_path).expect("Failed comprehending the csv: ");
     println!("{:?}", tasks);
     println!("first: {:?}", tasks[1]);
+
+    let new_task = Task {
+        active: true,
+        title: "string3".to_string(),
+        description: "string3".to_string(),
+        updated_on: "2024-04-20".to_string(),
+        created_on: "2024-02-20".to_string(),
+        created_by: "21".to_string(),
+        updated_by: "12".to_string(),
+    };
+
+    match add_task(&mut tasks, new_task) {
+        Ok(_) => println!("added successfully."),
+        Err(e) => println!("error adding task {e}"),
+    }
+
+    println!("tasks after adding: {:?}", tasks);
     Ok(())
 }
